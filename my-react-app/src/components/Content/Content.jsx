9f61d6dce809a1e3262e357f76ccc48215ec1b6a
Pavlo <galatovpasa82@gmail.com>
2025-01-23 22:18:08 +0200

    Done

---
+++ my-react-app/src/components/Content/Content.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Content.css";
+
+const Content = () => (
+    <div className="content">
+        <p>Центральний контейнер з довільним контентом.</p>
+    </div>
+);
+
+export default Content;
---
+++ my-react-app/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Sidebar.css";
+
+const Sidebar = () => (
+    <nav className="sidebar">
+        <ul>
+            <li>Посилання 1</li>
+            <li>Посилання 2</li>
+            <li>Посилання 3</li>
+        </ul>
+    </nav>
+);
+
+export default Sidebar;
